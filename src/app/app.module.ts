e9ab10728986bff633eac7951e2e774666f67876
Ankur Sachdeva <ankur.sachdeva2128@gmail.com>
2022-07-24 11:20:58 +0530

    Initial Commit - Adding Angular Project with Admin LTE 3 Template

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    MainFooterComponent,
+    ControlSidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
